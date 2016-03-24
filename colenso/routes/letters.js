2e67aa1883574f1fb8223e7db67b456841f04d69
Tim <tim.bathgate@gmail.com>
2016-03-24 17:40:05 +1300

    Implement download

--- colenso/routes/letters.js
+++ colenso/routes/letters.js
@@ -13,14 +13,38 @@ var query = `
 
 /* GET users listing. */
 router.get('/:letter', function(req, res, next) {
-
+  console.log(next);
   var interpolatedQuery = query.replace("%LETTERID%", req.params.letter);
   console.log(query);
   var xmlData = client.execute(interpolatedQuery, function(error, result){
 
     if (!error) {
-      res.render('letters', {data: result.result});
+      res.render('letters', {data: result.result, letterID: req.params.letter});
 
+    } else {
+      console.log(error);
+    }
+
+  });
+});
+
+var filepathQuery = `
+  XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';
+  for $letter in db:open("Colenso")
+  where contains(document-uri($letter), "%LETTERID%")
+  return document-uri($letter)
+`
+
+router.get('/:letter/download', function(req, res, next){
+  var filename = req.params.letter;
+  console.log("Trying to download " + filename);
+
+  var interpolatedQuery = filepathQuery.replace("%LETTERID%", req.params.letter);
+  client.execute(interpolatedQuery, function(error, result){
+
+    if (!error) {
+      console.log(result.result);
+      res.download("public/" + result.result, req.params.letter + ".xml");
     } else {
       console.log(error);
     }
