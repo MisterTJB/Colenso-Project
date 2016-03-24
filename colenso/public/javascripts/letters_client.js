85e6de7915bbdfcdf467ab6ce54a8ae22fab7209
Tim <tim.bathgate@gmail.com>
2016-03-25 12:27:43 +1300

    Fix HTML/TEI <title> name clash

---
+++ colenso/public/javascripts/letters_client.js
@@ -0,0 +1,3 @@
+// Replace <title> with <teititle> to prevent conflict with HTML <title>
+$("body tei teiheader filedesc titlestmt title").replaceWith('<teititle>' +
+ $('body tei teiheader filedesc titlestmt title').html() +'</teititle>');
