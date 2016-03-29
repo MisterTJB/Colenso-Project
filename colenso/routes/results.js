cdcbf54a66f11f95f3c49c732dece019288f76f3
Tim <tim.bathgate@gmail.com>
2016-03-29 19:20:27 +1300

    Fix string replace bug in query expansion

--- colenso/routes/results.js
+++ colenso/routes/results.js
@@ -7,13 +7,19 @@ var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 var namespace = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';";
 
 function formatResults(parsedResults){
+  var uniqueIDs = new Set();
   var formattedResults = [];
   for (var i=0; i < parsedResults.length; i++){
     var result = parsedResults[i][1];
-    var uriComponents = result.uri.split("/");
-    var documentIdentifier = uriComponents[uriComponents.length - 1].replace('.xml', '');
-    result.uri = documentIdentifier;
-    formattedResults.push(result)
+    console.log(result);
+    result.databaseURI = result.uri
+    if (!uniqueIDs.has(result.databaseURI)){
+      var uriComponents = result.uri.split("/");
+      var documentIdentifier = uriComponents[uriComponents.length - 1].replace('.xml', '');
+      result.uri = documentIdentifier;
+      uniqueIDs.add(result.databaseURI);
+      formattedResults.push(result)
+    }
   }
   return formattedResults;
 }
@@ -27,18 +33,18 @@ function basicSearch(searchTerms){
   searchString = ` json:serialize(
   <results>{
     let $colenso := fn:collection("Colenso")
-    let $matches := $colenso//text()[. contains text 'Darwin']
+    let $matches := $colenso//text()[. contains text "%SEARCH_TERMS%"]
     for $match in $matches
 
     let $uri := base-uri($match)
     let $title := doc($uri)//title
     let $author := doc($uri)/TEI/teiHeader/fileDesc/titleStmt/author/name
-    let $score := ft:score($match[. contains text 'Darwin'])
+    let $score := ft:score($match[. contains text "%SEARCH_TERMS%"])
     let $sent :=  doc($uri)//correspAction[@type = "sent"]/date/text()
     let $created := doc($uri)//creation/date/@when
     let $wordCount := count(ft:tokenize(doc($uri)))
 
-    let $context := ft:extract($match[. contains text 'Darwin'], "b")
+    let $context := ft:extract($match[. contains text "%SEARCH_TERMS%"], "b")
   order by $score descending
   return
     <result
@@ -51,7 +57,7 @@ function basicSearch(searchTerms){
           created="{$created}"
           wordCount="{$wordCount}"/>
     }</results>, map{'format': 'jsonml'})`
-  return namespace + searchString.replace("%SEARCH_TERMS%", searchTerms);
+  return namespace + searchString.replace(/%SEARCH_TERMS%/g, searchTerms);
 }
 
 function advancedSearch(xquery){
@@ -73,22 +79,17 @@ function advancedSearch(xquery){
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log(req.query);
-  client.execute("OPEN Colenso");
 
   var query = basicSearch(req.query.q);
   if (req.query.type === 'advanced') {
     query = advancedSearch(req.query.q);
   }
 
-  console.log(query);
   client.execute(query, function(error, result){
     if (!error) {
-
       var parsedResults = JSON.parse(result.result).slice(2);
       var formattedResults = formatResults(parsedResults);
-
-      res.render('results', {query: req.query.q, data: formattedResults, results: formattedResults.length});
+      res.render('results', {query: req.query, data: formattedResults, results: formattedResults.length});
 
     } else {
       console.log(error);
