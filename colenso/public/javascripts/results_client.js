48c64255848f4b32f6c45b45ce53212b851115b9
Tim <tim.bathgate@gmail.com>
2016-03-29 16:17:22 +1300

    Add advanced results display

--- colenso/public/javascripts/results_client.js
+++ colenso/public/javascripts/results_client.js
@@ -2,3 +2,27 @@
 // Replace <title> with <teititle> to prevent conflict with HTML <title>
 $("body tei teiheader filedesc titlestmt title").replaceWith('<teititle>' +
  $('body tei teiheader filedesc titlestmt title').html() +'</teititle>');
+
+$(document).ready(function(){
+  $(".resultAuthorCreated").hide();
+  $(".resultWordCount").hide();
+  $(".resultContext").hide();
+  $(".resultsCount").hide();
+  $(".searchTerm").hide();
+
+});
+
+$("#resultType").click(function(){
+  $(".resultAuthorCreated").toggle();
+  $(".resultWordCount").toggle();
+  $(".resultContext").toggle();
+  $(".resultsCount").toggle();
+  $(".searchTerm").toggle();
+
+  if ($("#resultType").text() === "Simple"){
+    $("#resultType").text("Advanced");
+  } else {
+    $("#resultType").text("Simple");
+  }
+
+});
--- colenso/routes/results.js
+++ colenso/routes/results.js
@@ -26,15 +26,30 @@ function basicSearch(searchTerms){
 
   searchString = ` json:serialize(
   <results>{
-  for $letter in db:open("Colenso")
-  where $letter[//text() contains text '%SEARCH_TERMS%' using wildcards]
+    let $colenso := fn:collection("Colenso")
+    let $matches := $colenso//text()[. contains text 'Darwin']
+    for $match in $matches
 
+    let $uri := base-uri($match)
+    let $title := doc($uri)//title
+    let $author := doc($uri)/TEI/teiHeader/fileDesc/titleStmt/author/name
+    let $score := ft:score($match[. contains text 'Darwin'])
+    let $sent :=  doc($uri)//correspAction[@type = "sent"]/date/text()
+    let $created := doc($uri)//creation/date/@when
+    let $wordCount := count(ft:tokenize(doc($uri)))
+
+    let $context := ft:extract($match[. contains text 'Darwin'], "b")
+  order by $score descending
   return
-
-        <result
-          title="{$letter//title/text()}"
-          author="{$letter/TEI/teiHeader/fileDesc/titleStmt/author/name/text()}"
-          uri="{document-uri($letter)}"/>
+    <result
+          title="{$title}"
+          author="{$author}"
+          uri="{$uri}"
+          context="{$context}"
+          score="{$score}"
+          sent="{$sent}"
+          created="{$created}"
+          wordCount="{$wordCount}"/>
     }</results>, map{'format': 'jsonml'})`
   return namespace + searchString.replace("%SEARCH_TERMS%", searchTerms);
 }
@@ -73,7 +88,7 @@ router.get('/', function(req, res, next) {
       var parsedResults = JSON.parse(result.result).slice(2);
       var formattedResults = formatResults(parsedResults);
 
-      res.render('results', {data: formattedResults});
+      res.render('results', {query: req.query.q, data: formattedResults, results: formattedResults.length});
 
     } else {
       console.log(error);
